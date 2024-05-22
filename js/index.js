03d9a71c2cc3ea04a4f12e8e04f8d98a88c08a99
20syl <77191035+20syldev@users.noreply.github.com>
2024-05-22 14:51:52 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Animation du bouton sponsor
     const sponsorImage = document.getElementById('sponsor');
 
     sponsorImage.addEventListener('mouseover', function() {
